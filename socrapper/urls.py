c5aa2a415f3a97a94fcc90381b36dc9e3ca4347d
sharkjira99@gmail.com <sharkjira99@gmail.com>
2026-06-13 22:33:13 +0700

    Add Google Search Console verification endpoint

--- socrapper/urls.py
+++ socrapper/urls.py
@@ -8,6 +8,9 @@ def robots_txt(request):
     content = "User-agent: *\nAllow: /\nDisallow: /api/\nDisallow: /admin/\n\nSitemap: https://www.socrapper.my.id/sitemap.xml\n"
     return HttpResponse(content, content_type='text/plain')
 
+def google_verification(request):
+    return HttpResponse("google-site-verification: google1289e1e5d73483a9.html\n", content_type='text/plain')
+
 def sitemap_xml(request):
     content = """<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -24,6 +27,7 @@ urlpatterns = [
     path('', api_root),
     path('robots.txt', robots_txt),
     path('sitemap.xml', sitemap_xml),
+    path('google1289e1e5d73483a9.html', google_verification),
     path('api/', include('scraper.urls')),
     path('api/', include('surfer.urls')),
 ]
